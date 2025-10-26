d927fdd58a6763c5d0b000bfd069beefd0bada18
muhittinaliakyuz1 <akyuzhome.ali21@gmail.com>
2025-10-26 17:27:13 +0300

    asd

--- src/main/java/com/example/mixin/HeartUseMixin.java
+++ src/main/java/com/example/mixin/HeartUseMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
